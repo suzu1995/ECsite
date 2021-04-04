4dbb04a7ed92dfdbe77eff26be364b7393bd8f42
suzu1995 <suzu.chao@gmail.com>
2021-04-04 17:44:11 +0900

    staffデータedit処理実装

---
+++ staff/staff_edit_check.php
@@ -0,0 +1,47 @@
+<body>
+<?php
+
+//入力フォームから値を取得
+$staff_code = $_POST['code'];
+$staff_name = $_POST['name'];
+$staff_pass = $_POST['pass'];
+$staff_pass2 = $_POST['pass2'];
+
+//表示文字列のエスケープ処理
+$staff_code = htmlspecialchars($staff_code,ENT_QUOTES, 'UTF-8');
+$staff_name = htmlspecialchars($staff_name,ENT_QUOTES, 'UTF-8');
+$staff_pass = htmlspecialchars($staff_pass,ENT_QUOTES, 'UTF-8');
+$staff_pass2 = htmlspecialchars($staff_pass2,ENT_QUOTES, 'UTF-8');
+
+if($staff_name == ''){
+    print'スタッフ名が入力されていません。<br />';
+}else{
+    print'スタッフ名';
+    print $staff_name;
+    print'<br />';
+}
+
+if($staff_pass == ''){
+    print 'パスワードが入力されていません<br />';
+}
+
+if($staff_pass !== $staff_pass2){
+    print 'パスワードが一致していません <br />';
+}
+
+if($staff_name == ''|| $staff_pass == ''|| $staff_pass !==$staff_pass2){
+    print '<form>';
+    print '<input type ="button" onclick ="history.back()" value ="戻る">';
+}else{
+    $staff_pass = md5($staff_pass);//暗号化
+    print '<form method ="post" action ="staff_edit_done.php">';
+    print '<input type="hidden" name ="code" value ="'.$staff_code.'">';
+    print '<input type="hidden" name ="name" value ="'.$staff_name.'">';
+    print '<input type="hidden" name ="pass" value ="'.$staff_pass.'">';
+    print '<br />';
+    print '<input type ="button" onclick ="history.back()" value ="戻る">';
+    print '<input type ="submit" value ="OK">';
+    print '</form>';
+}
+?>
+</body>
---
+++ staff/staff_edit_done.php
@@ -0,0 +1,43 @@
+<body>
+
+<?php
+
+try
+{
+
+    $staff_code = $_POST['code'];
+    $staff_name = $_POST['name'];
+    $staff_pass = $_POST['pass'];
+
+    $staff_code = htmlspecialchars($staff_code,ENT_QUOTES,'UTF-8');
+    $staff_name = htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
+    $staff_pass = htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
+
+    $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+
+    $user = 'suzu';
+    $password ='u9oIjVjw1dSXbaPp';
+    $dbh = new PDO($dsn,$user,$password);
+    
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = 'UPDATE m_staff SET name=?,password=? WHERE code=?';
+    $stmt = $dbh->prepare($sql);
+    $data[] = $staff_name;
+    $data[] = $staff_pass;
+    $data[] = $staff_code;
+    $stmt->execute($data);
+
+    $dbh = null;
+}
+catch(Exception $e)
+{
+    print 'ただいま障害により大変ご迷惑をおかけしております';
+    exit();
+}
+
+?>
+修正しました<br />
+<br />
+<a href="staff_list.php">戻る</a>
+
+</body>
