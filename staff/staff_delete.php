d6bd527a2b051e090acd5efca1d4640165483148
suzu1995 <suzu.chao@gmail.com>
2021-04-18 23:33:25 +0900

    削除機能実装

---
+++ staff/staff_delete.php
@@ -0,0 +1,45 @@
+<body>
+<?php
+
+try
+{
+    $staff_code = $_GET['staffcode'];
+
+    $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+    $user = 'suzu';
+    $password ='u9oIjVjw1dSXbaPp';
+    $dbh = new PDO($dsn, $user,$password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    $sql = 'SELECT code,name FROM m_staff WHERE code=?';
+    $stmt = $dbh->prepare($sql);//SQL文のセット
+    $data[]=$staff_code;
+    $stmt->execute($data);//実行
+
+    $record = $stmt->fetch(PDO::FETCH_ASSOC);
+    $staff_name = $record['name'];
+    $dbh = null;
+}
+catch(Exception $e)
+{
+    print 'ただいま障害により大変ご迷惑をおかけしています。';
+    exit();
+}
+?>
+
+スタッフ削除<br />
+<br />
+スタッフコード<br />
+<?php print $staff_code;?>
+<br />
+スタッフ名<br />
+<?php print $staff_name;?>
+<br />
+このスタッフを削除してよろしいですか？<br />
+<br />
+<form method="post" action="staff_delete_done.php">
+<input type="hidden" name="code" value="<?php print $staff_code;?>">
+<input type="button" onclick="history.back()" value="戻る">
+<input type="submit" value="OK">
+</form>
+</body>
---
+++ staff/staff_delete_done.php
@@ -0,0 +1,37 @@
+<body>
+
+<?php
+
+try
+{
+
+    $staff_code = $_POST['code'];
+
+    $staff_code = htmlspecialchars($staff_code,ENT_QUOTES,'UTF-8');
+
+    $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+
+    $user = 'suzu';
+    $password ='u9oIjVjw1dSXbaPp';
+    $dbh = new PDO($dsn,$user,$password);
+    
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = 'DELETE FROM m_staff WHERE code=?';
+    $stmt = $dbh->prepare($sql);
+    $data[] = $staff_code;
+    $stmt->execute($data);
+
+    $dbh = null;
+}
+catch(Exception $e)
+{
+    print 'ただいま障害により大変ご迷惑をおかけしております';
+    exit();
+}
+
+?>
+削除しました<br />
+<br />
+<a href="staff_list.php">戻る</a>
+
+</body>
