834fc16df4b26178b08664452d116938f8c43ec6
suzu1995 <suzu.chao@gmail.com>
2021-05-03 00:25:24 +0900

    商品追加画面に画像の項目追加

--- product/product_add.php
+++ product/product_add.php
@@ -7,11 +7,13 @@
 <body>
 商品追加<br />
 <br />
-<form method="post" action="product_add_check.php">
+<form method="post" action="product_add_check.php" enctype="multipart/form-data">
 商品名を入力してください<br />
 <input type="text" name="name" style="width: 200px;"><br />
 価格を入力してください<br />
 <input type="text" name="price" style="width: 100px"><br />
+画像を選んでください<br />
+<input type ="file" name="gazou" style="width: 400px;"><br />
 <input type="button" onclick="history.back()" value="戻る">
 <input type="submit" value="OK">
 </form>
