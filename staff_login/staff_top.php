d4a8c438c76a4e6d5d2972ef5ba95c274323de4a
suzu1995 <suzu.chao@gmail.com>
2021-05-16 20:41:12 +0900

    ログアウト機能実装

--- staff_login/staff_top.php
+++ staff_login/staff_top.php
@@ -22,5 +22,7 @@ if(isset($_SESSION['login'])==false){
 <br />
 <a href="../staff/staff_list.php">スタッフ管理</a><br />
 <a href="../product/product_list.php">商品管理</a><br />
+<br />
+<a href="staff_logout.php">ログアウト</a><br />
 </body>
 </html>
