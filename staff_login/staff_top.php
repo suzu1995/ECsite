3aa3091226bf3e4f6da69ca92c2ad8410730558f
suzu1995 <suzu.chao@gmail.com>
2021-05-16 17:56:56 +0900

    セッション機能追加して ログインしていない場合にアクセスできるように作成

--- staff_login/staff_top.php
+++ staff_login/staff_top.php
@@ -1,4 +1,26 @@
+<?php
+session_start();
+session_regenerate_id(true);
+if(isset($_SESSION['login'])==false){
+    print 'ログインされていません。<br />';
+    print '<a href=../staff_login/staff_login.html>ログイン画面へ</a>';
+    exit();
+}else{
+    print $_SESSION['staff_name'];
+    print 'さんログイン中<br />';
+    print '<br />';
+}
+?>
+<!DECOTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ろくまる農園</title>
+</head>
+<body>
 ショップ管理トップメニュー<br />
 <br />
 <a href="../staff/staff_list.php">スタッフ管理</a><br />
 <a href="../product/product_list.php">商品管理</a><br />
+</body>
+</html>
