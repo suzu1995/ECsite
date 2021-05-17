c49afe1eae2cfdb931560f52f271aa7c3ab5009c
suzu1995 <suzu.chao@gmail.com>
2021-05-18 00:11:45 +0900

    ファイルの共通化

--- asobi/gengo.php
+++ asobi/gengo.php
@@ -8,26 +8,10 @@
 
 <?php
 
+require_once('../common/common.php');
+
 $seireki = $_POST['seireki'];
 $wareki = gengo($seireki);
 print $wareki;
 
-function gengo($seireki){
-    if(1868<=$seireki && $seireki<=1911){
-        $gengo = '明治';
-    }
-
-    if(1912<=$seireki && $seireki<=1925){
-        $gengo = '大正';
-    }
-
-    if(1926<=$seireki && $seireki<=1988){
-        $gengo = '昭和';
-    }
-
-    if(1989<=$seireki && $seireki<=2019){
-        $gengo = '平成';
-    }
-    return($gengo);
-}
 ?>
