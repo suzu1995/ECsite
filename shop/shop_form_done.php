5f772f2c1b789f062a5515299ca8fda337a8243d
suzu1995 <suzu.chao@gmail.com>
2021-05-27 23:38:18 +0900

    注文受付画面実装

---
+++ shop/shop_form_done.php
@@ -0,0 +1,30 @@
+<!DECOTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ろくまる農園</title>
+</head>
+<body>
+
+<?php
+require_once('../common/common.php');
+
+$post = sanitize($_POST);
+
+$onamae = $post['onamae'];
+$email = $post['email'];
+$postal1 = $post['postal1'];
+$postal2 = $post['postal2'];
+$address = $post['address'];
+$tel = $post['tel'];
+
+print $onamae.'様<br />';
+print '注文ありがとうございました。<br />';
+print $email.'にメールを送りましたのでご確認ください<br /';
+print '商品は以下の住所に発送させていただきます。<br />';
+print $postal1.'-'.$postal2.'<br />';
+print $address.'<br />';
+print $tel.'<br />';
+?>
+</body>
+</html>
