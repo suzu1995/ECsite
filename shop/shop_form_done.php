d48f76fe901821396d8ea664a723596848dbb4a3
suzu1995 <suzu.chao@gmail.com>
2021-05-28 23:06:40 +0900

    セッションハイジャック対策、DBエラー障害対策

--- shop/shop_form_done.php
+++ shop/shop_form_done.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+    session_regenerate_id();
+?>
 <!DECOTYPE html>
 <html>
 <head>
@@ -7,24 +11,31 @@
 <body>
 
 <?php
-require_once('../common/common.php');
+try{
+    require_once('../common/common.php');
 
-$post = sanitize($_POST);
+    $post = sanitize($_POST);
 
-$onamae = $post['onamae'];
-$email = $post['email'];
-$postal1 = $post['postal1'];
-$postal2 = $post['postal2'];
-$address = $post['address'];
-$tel = $post['tel'];
+    $onamae = $post['onamae'];
+    $email = $post['email'];
+    $postal1 = $post['postal1'];
+    $postal2 = $post['postal2'];
+    $address = $post['address'];
+    $tel = $post['tel'];
 
-print $onamae.'様<br />';
-print '注文ありがとうございました。<br />';
-print $email.'にメールを送りましたのでご確認ください<br /';
-print '商品は以下の住所に発送させていただきます。<br />';
-print $postal1.'-'.$postal2.'<br />';
-print $address.'<br />';
-print $tel.'<br />';
+    print $onamae.'様<br />';
+    print '注文ありがとうございました。<br />';
+    print $email.'にメールを送りましたのでご確認ください<br /';
+    print '商品は以下の住所に発送させていただきます。<br />';
+    print $postal1.'-'.$postal2.'<br />';
+    print $address.'<br />';
+    print $tel.'<br />';
+
+}catch(Exception $e){
+    print 'ただいま障害により大変ご迷惑をおかけしています';
+    exit();
+}
+
 ?>
 </body>
 </html>
